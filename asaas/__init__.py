b7f259ec7f6b947d1bb1f9299a3db8c266b2d752
Fernando Leite <fernando.leite@wlcsolucoes.com.br>
2024-06-26 15:47:10 -0300

    Add Costumers class for managing customer data and requests

--- asaas/__init__.py
+++ asaas/__init__.py
@@ -85,3 +85,168 @@ class Asaas:
         raise_for_status(response)
 
         return response
+
+
+class Costumers:
+    def __init__(self, asaas: Asaas):
+        self.asaas = asaas
+
+    def retrieve(
+        self,
+        customer_id: str
+    ) -> Customer:
+        """Retrieve a customer by ID"""
+
+        response = self.asaas.get(f'customers/{customer_id}')
+
+        return Customer(**response.json())
+
+    def create(
+        self,
+        name: str,
+        cpfCnpj: str,
+        email: Optional[str] = None,
+        phone: Optional[str] = None,
+        mobilePhone: Optional[str] = None,
+        address: Optional[str] = None,
+        addressNumber: Optional[str] = None,
+        complement: Optional[str] = None,
+        province: Optional[str] = None,
+        postalCode: Optional[str] = None,
+        externalReference: Optional[str] = None,
+        notificationDisabled: Optional[bool] = None,
+        additionalEmails: Optional[str] = None,
+        municipalInscription: Optional[str] = None,
+        stateInscription: Optional[str] = None,
+        observations: Optional[str] = None,
+        groupName: Optional[str] = None,
+        company: Optional[str] = None
+    ) -> Customer:
+        """Create a new customer"""
+
+        data = remove_none_and_empty_values(
+            {
+                'name': name,
+                'cpfCnpj': cpfCnpj,
+                'email': email,
+                'phone': phone,
+                'mobilePhone': mobilePhone,
+                'address': address,
+                'addressNumber': addressNumber,
+                'complement': complement,
+                'province': province,
+                'postalCode': postalCode,
+                'externalReference': externalReference,
+                'notificationDisabled': notificationDisabled,
+                'additionalEmails': additionalEmails,
+                'municipalInscription': municipalInscription,
+                'stateInscription': stateInscription,
+                'observations': observations,
+                'groupName': groupName,
+                'company': company
+            }
+        )
+
+        response = self.asaas.post('customers', data)
+
+        return Customer(**response.json())
+
+    def list(
+        self,
+        name: Optional[str] = None,
+        email: Optional[str] = None,
+        cpfCnpj: Optional[str] = None,
+        groupName: Optional[str] = None,
+        externalReference: Optional[str] = None,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None
+    ) -> list[Customer]:
+        """List customers"""
+
+        params = remove_none_and_empty_values(
+            {
+                'offset': offset,
+                'limit': limit,
+                'name': name,
+                'email': email,
+                'cpfCnpj': cpfCnpj,
+                'groupName': groupName,
+                'externalReference': externalReference
+            }
+        )
+
+        response = self.asaas.get('customers', params)
+
+        return [Customer(**customer) for customer in response.json()['data']]
+
+    def update(
+        self,
+        customer_id: str,
+        name: str,
+        cpfCnpj: str,
+        email: Optional[str] = None,
+        phone: Optional[str] = None,
+        mobilePhone: Optional[str] = None,
+        address: Optional[str] = None,
+        addressNumber: Optional[str] = None,
+        complement: Optional[str] = None,
+        province: Optional[str] = None,
+        postalCode: Optional[str] = None,
+        externalReference: Optional[str] = None,
+        notificationDisabled: Optional[bool] = None,
+        additionalEmails: Optional[str] = None,
+        municipalInscription: Optional[str] = None,
+        stateInscription: Optional[str] = None,
+        observations: Optional[str] = None,
+        groupName: Optional[str] = None,
+        company: Optional[str] = None
+    ) -> Customer:
+        """Update a customer by ID"""
+
+        data = remove_none_and_empty_values(
+            {
+                'name': name,
+                'cpfCnpj': cpfCnpj,
+                'email': email,
+                'phone': phone,
+                'mobilePhone': mobilePhone,
+                'address': address,
+                'addressNumber': addressNumber,
+                'complement': complement,
+                'province': province,
+                'postalCode': postalCode,
+                'externalReference': externalReference,
+                'notificationDisabled': notificationDisabled,
+                'additionalEmails': additionalEmails,
+                'municipalInscription': municipalInscription,
+                'stateInscription': stateInscription,
+                'observations': observations,
+                'groupName': groupName,
+                'company': company
+            }
+        )
+
+        response = self.asaas.put(
+            f'customers/{customer_id}',
+            data
+        )
+
+        return Customer(**response.json())
+
+    def delete(
+        self,
+        customer_id: str
+    ) -> None:
+        """Delete a customer by ID"""
+
+        self.asaas.delete(f'customers/{customer_id}')
+
+    def restore(
+        self,
+        customer_id: str
+    ) -> Customer:
+        """Restore a customer by ID"""
+
+        response = self.asaas.post(f'customers/{customer_id}/restore')
+        
+        return Customer(**response.json())
