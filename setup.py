4d515fb58ccf2f7e538899e5e3c5358cd37b9e30
Fernando Leite <fernando.leite@wlcsolucoes.com.br>
2024-07-10 17:37:48 -0300

    chore: Bump version to 0.1.6 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
 setup(
     name='asaas-sdk-wlc',
     packages=find_packages(include=['asaas']),
-    version='0.1.5',
+    version='0.1.6',
     license='Apache-2.0',
     description='SDK for Asaas API',
     author='WLC Soluções',
